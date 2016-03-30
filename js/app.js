dd8fb940cd9844e7947d3f9608465f33a8f5e943
Tobias Årud <me@tobiasarud.se>
2016-03-30 21:35:51 +0200

    day 1 code
    ugly jquery, and font icons added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 $(document).foundation();
 
 // Code by Tobias Årud
+$(document).ready(function(){
+    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&user=Erumaro87&api_key=d7c4e66cb9d80c4c8dede52e9805d669&format=json&callback=?", function(json){
+        var html = "";  
+        
+        var backgroundnewURL = json.toptracks.track[0].image[3]["#text"];
+        
+        $.each(json.toptracks.track, function(i, item) {  
+            /*
+            html += "<p><a href=" + item.url + " target='_blank'>" + item.name + " - " + "Play count : " +item.playcount + "</a></p>";
+            */
+            html += "<ul class='row'><li class='large-1 columns'><span class='list-order'>" + item["@attr"].rank + "</span></li><li class='large-2 columns'><img src="+ item.image[2]["#text"] +" /></li><li class='large-4 columns'><a href="+ item.artist.url +">"+ item.artist.name +"</a> - <a href="+ item.url +">"+ item.name +"</a></li><li class='large-4 columns'>" + item.playcount + "</li><li class='large-1 columns'><span class='list-order'>" + item.streamable["#text"] + "</span></li></ul><br>";
+        });
+        /*
+        $("body").css({
+            "background-image": "url('"+ backgroundnewURL +"')"
+        });
+        */
+        $("#chart").append(html);
+    });
+});
